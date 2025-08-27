0dbba1c8207340f244e7b87de46d32a7eef4c4c2
d.chertkov <d.chertkov@getproduction.ru>
2025-08-27 13:41:07 +0500

    add product_page

--- src/js/sliders/sliders.js
+++ src/js/sliders/sliders.js
@@ -154,3 +154,45 @@
 	// 		},
 	// 	  },
 	//   });
+
+
+
+var cardSliderNav = new Swiper(".product_page__main--slider--nav", {
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    freeMode: true,
+	direction: "vertical",
+    watchSlidesProgress: true,	
+});
+var cardSliderMain = new Swiper(".product_page__main--slider--main", {
+    spaceBetween: 10,
+	navigation: {
+        nextEl: ".product_page__main--slider--main--nav--next",
+        prevEl: ".product_page__main--slider--main--nav--prev",
+    },
+	pagination: {
+        el: ".product_page__main--slider--main--pagination",
+        clickable: true,
+    },
+    thumbs: {
+        swiper: cardSliderNav,
+    },
+});
+
+cardSliderMain.on('slideChange', () => {
+  	cardSliderNav.slideTo(cardSliderMain.activeIndex, 500, false);
+});
+var visibleSlides = 6;
+
+cardSliderMain.on('slideChange', () => {
+    const activeIndex = cardSliderMain.activeIndex;
+    const navIndex = cardSliderNav.activeIndex;
+
+    if (activeIndex > navIndex + visibleSlides - 1) {
+        cardSliderNav.slideTo(navIndex + 1); 
+    }
+
+    if (activeIndex <= navIndex) {
+        cardSliderNav.slideTo(navIndex - 1);
+    }
+});
